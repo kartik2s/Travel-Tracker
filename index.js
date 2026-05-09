e2b8beb4730e043050960c0567cb2ceb9917998c
Kartik Sharma <ks884024@gmail.com>
2026-05-10 04:41:03 +0530

    updates users

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ db.connect();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+let currentUserId = 1;
+let users = [
+    { id: 1, name: "Angela", color: "teal" }
+];
+
 async function checkVisited() {
-    const result = await db.query("SELECT country_code FROM visited_countries");
+    const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;", [currentUserId]);
     let countries = [];
     result.rows.forEach((country) => {
         countries.push(country.country_code);
@@ -26,15 +31,26 @@ async function checkVisited() {
     return countries;
 }
 
+async function getCurrentUser() {
+    const result = await db.query("SELECT * FROM users");
+    users = result.rows;
+    return users.find((user) => user.id == currentUserId);
+}
+
 app.get("/", async (req, res) => {
     const countries = await checkVisited();
+    const currentUser = await getCurrentUser();
     console.log(countries);
     res.render("index.ejs", {
-        countries: countries, total: countries.length
+        countries: countries,
+        total: countries.length,
+        users: users,
+        color: currentUser.color
     });
 });
 
 app.post("/add", async (req, res) => {
+    const currentUser = await getCurrentUser();
     try {
         const country = req.body.country;
         const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'", [country.toLowerCase()]);
@@ -43,7 +59,7 @@ app.post("/add", async (req, res) => {
         console.log(data);
         const countryCode = data.country_code;
         try {
-            await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+            await db.query("INSERT INTO visited_countries (country_code, user_id) VALUES ($1,$2)", [countryCode, currentUserId]);
             res.redirect("/");
         } catch (error) {
             console.log(error);
@@ -51,6 +67,8 @@ app.post("/add", async (req, res) => {
             res.render("index.ejs", {
                 countries: countries,
                 total: countries.length,
+                users: users,
+                color: currentUser.color,
                 error: "Country has already been added, try again"
             });
         }
@@ -60,9 +78,33 @@ app.post("/add", async (req, res) => {
         res.render("index.ejs", {
             countries: countries,
             total: countries.length,
+            users: users,
+            color: currentUser.color,
             error: "Country name does not exit, try again"
         });
     }
+});
+
+app.post("/user", async (req, res) => {
+    if (req.body.add === "new") {
+        res.render("new.ejs");
+    } else {
+        currentUserId = req.body.user;
+        res.redirect("/");
+    }
+});
+
+app.post("/new", async (req, res) => {
+    const name = req.body.name;
+    const color = req.body.color;
+
+    const result = await db.query("INSERT INTO users (name, color) VALUES ($1, $2) RETURNING *;",
+        [name, color]
+    );
+    const id = result.rows[0].id;
+    currentUserId = id;
+
+    res.redirect("/");
 });
 
 app.listen(port, () => {
